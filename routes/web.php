f8a8229e4d827d53a165c09f75a8963fa3bb71b0
R-O-M-CODE <rafiumoshoodolakunle@gmail.com>
2020-05-14 06:17:45 +0100

    Using routes

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "About US";
+});
+
+
+Route::get('/contact', function () {
+    return "Contact Us";
+});
+
+Route::get('/post/{id}/{name}', function($id,$name){
+    return "This is post number ". $id ." published by ".$name;
+});
+
+// naming routes
+Route::get('admin/posts/example', array('as'=>'admin.home' ,function(){
+
+    $url = route('admin.home');
+
+    return "This url is ". $url;
+
+}));
